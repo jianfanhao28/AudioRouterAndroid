c3acd8ca38d56b682067a3420e6d0af64ceea061
jianfanhao28 <jianfanhao28@gmail.com>
2026-08-29 21:00:00 +0800

    Update build.gradle.kts for Java and Kotlin versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,11 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.jian.audioRouter"
     compileSdk = 35
+
     defaultConfig {
         applicationId = "com.jian.audioRouter"
         minSdk = 26
@@ -12,10 +14,23 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildTypes {
-        release { isMinifyEnabled = false }
+        release {
+            isMinifyEnabled = false
+        }
     }
 }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.activity:activity-ktx:1.10.0")
